6d94690e17e853f577ea7b46708409c8309e875b
afsoonaslanii <aslani.a7260@gmail.com>
2020-01-11 02:34:01 +0330

    [add][component][about-me] add about me component

---
+++ src/components/about-me/index.js
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react';
+
+function AboutMe() {
+  return (
+    <Fragment>
+      <header className="entry-header">
+        <h1 className="entry-title">summary</h1>
+      </header>
+
+
+      <div className='niko-sc niko-sc--about'>
+        <div className='niko-sc__about-description'>
+          I am a web developer and would love to take up challenging projects
+          My goal is work as a team with minded people. where I can ideate freely and use my knowledge to achieve
+          excellence experiences and build something truly remarkable i’m always interested in learning and probing
+        </div>
+      </div>
+
+    </Fragment>
+
+  );
+}
+
+export default AboutMe;
+
